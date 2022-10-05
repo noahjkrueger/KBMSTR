06a94e7593ee09b7268bd07de0f9eaa20ac8cd50
Noah Krueger <noahjkrueger@gmail.com>
2022-10-05 16:40:55 -0400

    prelim optimizations, classes

--- python/KBMSTR.py
+++ python/KBMSTR.py
@@ -7,89 +7,140 @@ from zipfile import ZipFile
 from datetime import datetime
 
 
-def measure_vs_dataset(keyboard_mapping, finger_duty, cost, dataset):
-    dist, chars, uncounted = 0, 0, 0
-    finger_pos = {"l_p": 10, "l_r": 11, "l_m": 12, "l_i": 13, "r_i": 16, "r_m": 17, "r_r": 18, "r_p": 19}
-    while line := dataset.readline():
-        # TODO: make better>
-        tokens = line.decode()[:-1].lower().split(" ")
-        for token in tokens:
-            for char in token:
-                if char in keyboard_mapping.keys():
-                    chars += 1
-                    trans_dest = keyboard_mapping[char]
-                    duty = finger_duty[trans_dest]
-                    transition = finger_pos[duty], trans_dest
-                    transition_i = trans_dest, finger_pos[duty]
-                    if transition[0] == transition_i[0]:
-                        pass
-                    elif transition in cost.keys():
-                        dist += cost[transition]
-                    else:
-                        dist += cost[transition_i]
-                    finger_pos[duty] = trans_dest
-                else:
-                    uncounted += 1
-    return dist, chars, uncounted
+class _KeyboardTool:
+    def __init__(self):
+        self.__layout = None
+        self.__mapping = None
+        self.__finger_duty = None
+        self.__finger_pos = None
+        self.__cost_matrix = None
+        self.__accumulated_cost = 0
+
+    def get_info(self):
+        return self.__layout, self.__accumulated_cost
+
+    def set_layout(self, layout):
+        self.__layout = layout
+        self._init_mapping(layout)
+        self._init_finger_duty()
+        self._init_finger_pos()
+        self._init_cost_matrix()
+
+# TODO : accept mapping and finger duty, cost mapping do it that way
+    def transition_and_cost(self, char):
+        if not self.__layout:
+            raise Exception("KeyboardTool: not initialized. use KeyboardTool.set_layout(layout)")
+        if char == " ":
+            return True
+        try:
+            destination = self.__mapping[char]
+        except KeyError:
+            return False
+        responsible_finger = self.__finger_duty[destination]
+        transition = (self._finger_pos[responsible_finger], destination)
+        if transition[0] == transition[1]:
+            return True
+        self._finger_pos[responsible_finger] = destination
+        try:
+            self.__accumulated_cost += self.__cost_matrix[transition]
+        except KeyError:
+            self.__accumulated_cost += self.__cost_matrix[(transition[1], transition[0])]
+        return True
+
+    def _init_mapping(self, layout):
+        i = 0
+        mapping = {}
+        for char in layout:
+            mapping[char] = i
+            i += 1
+        self.__mapping = mapping
+
+    def _init_finger_pos(self):
+        self._finger_pos = {"l_p": 10, "l_r": 11, "l_m": 12, "l_i": 13, "r_i": 16, "r_m": 17, "r_r": 18, "r_p": 19}
+
+    def _init_finger_duty(self):
+        finger_duty = dict.fromkeys([0, 10, 21], "l_p")
+        finger_duty.update(dict.fromkeys([1, 11, 22], "l_r"))
+        finger_duty.update(dict.fromkeys([2, 12, 23], "l_m"))
+        finger_duty.update(dict.fromkeys([3, 4, 13, 14, 24, 25], "l_i"))
+        finger_duty.update(dict.fromkeys([5, 6, 15, 16, 26, 27], "r_i"))
+        finger_duty.update(dict.fromkeys([7, 17, 28], "r_m"))
+        finger_duty.update(dict.fromkeys([8, 18, 29], "r_r"))
+        finger_duty.update(dict.fromkeys([9, 19, 20, 30], "r_p"))
+        self.__finger_duty = finger_duty
+
+    def _init_cost_matrix(self):
+        a, b, c, d, e, f, g = 1.0, 1.031, 2.062, 2.016, 1.118, 1.6, 2.5
+        cost = dict.fromkeys([(3, 4), (13, 14), (24, 25), (5, 6), (15, 16), (26, 27), (19, 20)], a)
+        cost.update(dict.fromkeys([(0, 10), (1, 11), (2, 12), (3, 13), (4, 14), (5, 15), (6, 16),
+                                   (7, 17), (8, 18), (9, 19), (10, 21), (11, 22), (12, 23), (13, 24),
+                                   (14, 25), (15, 26), (16, 27), (17, 28), (18, 29), (19, 30)], b))
+        cost.update(dict.fromkeys([(0, 21), (1, 22), (2, 23), (3, 24), (4, 25), (5, 26), (6, 27),
+                                   (7, 28), (8, 29), (9, 30)], c))
+        cost.update(dict.fromkeys([(4, 24), (6, 26)], d))
+        cost.update(dict.fromkeys([(4, 13), (6, 15), (14, 24), (16, 26), (20, 30)], e))
+        cost.update(dict.fromkeys([(3, 14), (13, 25), (5, 16), (15, 27), (9, 20)], f))
+        cost.update(dict.fromkeys([(3, 25), (5, 27)], g))
+        self.__cost_matrix = cost
+
+# TODO: use kbtool better -> all use same mapping
+class AnalyzeKeyboards:
+    def __init__(self):
+        self.__kb_tools = list()
+        self.__chars = 0
+        self.__uncounted = 0
+        self.__dataset_names = list()
+
+    def init_keyboards(self, keyboards):
+        for keyboard in keyboards:
+            tool = _KeyboardTool()
+            tool.set_layout(keyboard)
+            self.__kb_tools.append(tool)
+
+    def _process_line(self, string):
+        for char in string:
+            self.__chars += 1
+            for kb in self.__kb_tools:
+                if not kb.transition_and_cost(char):
+                    self.__uncounted += 1
+                    break
+
+    def preform_analysis(self, dataset):
+        if not self.__kb_tools:
+            raise Exception("no keyboards initalized. use AnalyzeKeyboards.init_keyboards(list)")
+        zips = []
+        for root, direct, files in os.walk(dataset):
+            for file in files:
+                if '.zip' in file:
+                    zips.append(os.path.join(root, file))
+        for zip_path in zips:
+            with ZipFile(zip_path, 'r') as zipObj:
+                for file in zipObj.namelist():
+                    if '.txt' in file:
+                        self.__dataset_names.append(f"{zip_path.split('/')[-1]}/{file}")
+                        with zipObj.open(file) as dataset:
+                            while line := dataset.readline():
+                                self._process_line(line.decode()[:-1].lower())
+
+    def get_results(self):
+        keyboards = {}
+        efficiencies = {}
+        for kb in self.__kb_tools:
+            layout, total_cost = kb.get_info()
+            keyboards[layout] = total_cost
+            efficiencies[layout] = (self.__chars - self.__uncounted) / total_cost
+        return {
+            'keyboards': keyboards,
+            'total_chars': self.__chars,
+            'total_uncounted': self.__uncounted,
+            'efficiencies': efficiencies,
+            'dataset_names': self.__dataset_names,
+            'last_analysis': datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+        }
 
 
-def get_keyboard_maps(keyboard_layout):
-    i = 0
-    keyboard_mapping = {}
-    for char in keyboard_layout:
-        keyboard_mapping[char] = i
-        i += 1
-
-    # TODO: check these
-    finger_duty = dict.fromkeys([0, 10], "l_p")
-    finger_duty.update(dict.fromkeys([1, 11, 21], "l_r"))
-    finger_duty.update(dict.fromkeys([2, 12, 22], "l_m"))
-    finger_duty.update(dict.fromkeys([3, 13, 23, 4, 14, 24, 25], "l_i"))
-    finger_duty.update(dict.fromkeys([5, 6, 15, 16, 26, 27], "r_i"))
-    finger_duty.update(dict.fromkeys([7, 17, 28], "r_m"))
-    finger_duty.update(dict.fromkeys([8, 18, 29], "r_r"))
-    finger_duty.update(dict.fromkeys([9, 19, 20, 30], "r_p"))
-
-    # TODO: check these
-    a, b, c, d, e, f, g, h, i, r = 1.0, 1.1, 1.2, 1.3, 1.5, 2.0, 2.3, 2.5, 2.75, 0.0
-    cost = dict.fromkeys([(3, 4), (23, 24), (24, 25), (5, 6), (26, 27), (19, 20)], a)
-    cost.update(dict.fromkeys([(0, 10), (1, 11), (2, 12), (3, 13), (4, 14), (6, 16), (7, 17), (8, 18), (9, 19)], b))
-    cost.update(dict.fromkeys([(11, 21), (12, 22), (13, 23), (13, 24), (15, 26), (16, 26), (16, 27),
-                                (17, 28), (18, 29), (19, 30), (20, 30)], c))
-    cost.update(dict.fromkeys([(4, 13)], d))
-    cost.update(dict.fromkeys([(13, 25), (9, 20), (3, 14)], e))
-    cost.update(dict.fromkeys([(1, 21), (2, 22), (3, 23), (4, 24), (6, 26)], f))
-    cost.update(dict.fromkeys([(3, 24), (4, 25), (5, 26), (6, 27), (9, 30)], g))
-    cost.update(dict.fromkeys([(4, 23), (5, 27)], h))
-    cost.update(dict.fromkeys([(3, 25)], h))
-    return keyboard_mapping, finger_duty, cost
-
-
-def analyze_keyboard(keyboard, datasets):
-    zips = []
-    for root, direct, files in os.walk(datasets):
-        for file in files:
-            if '.zip' in file:
-                zips.append(os.path.join(root, file))
-    total_distance, total_chars, total_uncounted, dataset_names = 0, 0, 0, []
-    keyboard_mapping, finger_duty, cost = get_keyboard_maps(keyboard['layout'])
-    for zip_path in zips:
-        with ZipFile(zip_path, 'r') as zipObj:
-            for file in zipObj.namelist():
-                if '.txt' in file:
-                    dataset_names.append(f"{zip_path.split('/')[-1]}/{file}")
-                    with zipObj.open(file) as dataset:
-                        dist, chars, uncounted = measure_vs_dataset(keyboard_mapping, finger_duty, cost, dataset)
-                        total_distance += dist
-                        total_chars += chars
-                        total_uncounted += uncounted
-    keyboard['total_distance'] = total_distance
-    keyboard['total_chars'] = total_chars
-    keyboard['total_uncounted'] = total_uncounted
-    keyboard['efficiency'] = total_chars / total_distance
-    keyboard['dataset_names'] = dataset_names
-    keyboard['last_analysis'] = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    return keyboard
+def generate_keyboards(simplify, original, dataset, gensize, epsilon, save_stats):
+    return {}, {}
 
 
 def show_keyboards(keyboard):
@@ -97,7 +148,9 @@ def show_keyboards(keyboard):
 
     def load_json_to_html(name, layout, last_analysis, efficiency, datasets):
         eel.read_data(name, layout, last_analysis, efficiency, datasets)
-    load_json_to_html(keyboard['name'], keyboard['layout'], keyboard['last_analysis'], keyboard['efficiency'], keyboard['dataset_names'])
+
+    load_json_to_html(keyboard['name'], keyboard['layout'], keyboard['last_analysis'], keyboard['efficiency'],
+                      keyboard['dataset_names'])
     eel.start('index.html', size=(1000, 700))
 
 
@@ -123,21 +176,18 @@ def main(argv):
              "Directories of multiple .zip collections are allowed. "
     )
     parser.add_argument(
-        "-result_type",
-        choices=['raw', 'simplified', 'both'],
-        default='both',
-        help="Select the type of result to output.\nraw: the most efficient keyboard in terms of distance for the "
-             "dataset\nsimplified: Not as effect as raw, but closer to the keyboard specified in the 'improve'"
-             " argument; selecting this will lessen the learning curve to adapt to a new keyboard layout\nboth: create "
-             "both\nResults are stored in /keyboards."
-    )
-    parser.add_argument(
         "-name",
         metavar="NAME",
         type=str,
         default="",
         help="Name the keyboard being generated. (.raw/.simplified) are added with respect to result_type. Default "
              "naming scheme: yyyy/mm/dd:hh:mm:ss.(raw/simplified)"
+    )
+    parser.add_argument(
+        "-simplify",
+        action='store_true',
+        help="Include a simplified version of the generated keyboard- more closely resembles the inputted keyboard. ("
+             "less efficiency metric, lower learning curver) "
     )
     parser.add_argument(
         "-gen_size",
@@ -170,18 +220,34 @@ def main(argv):
         help="Create a visual display of the keyboard inputted. Ignores all other options."
     )
     args = parser.parse_args(argv)
+
     with open(args.keyboard, "r") as json_file:
         keyboard = json.load(json_file)
     if args.display:
         show_keyboards(keyboard)
         exit()
     if args.analyze:
-        keyboard = analyze_keyboard(keyboard, args.dataset)
-    else:
-        pass # TODO
-        # generate_keyboard(keyboard, )
-    with open(args.keyboard, "w") as json_file:
-        json.dump(keyboard, json_file)
+        analysis = AnalyzeKeyboards()
+        analysis.init_keyboards([keyboard['layout']])
+        analysis.preform_analysis(args.dataset)
+        results = analysis.get_results()
+        keyboard['total_distance'] = results['keyboards'][keyboard['layout']]
+        keyboard['total_chars'] = results['total_chars']
+        keyboard['total_uncounted'] = results['total_uncounted']
+        keyboard['efficiency'] = results['efficiencies'][keyboard['layout']]
+        keyboard['dataset_names'] = results['dataset_names']
+        keyboard['last_analysis'] = results['last_analysis']
+        with open(args.keyboard, "w") as json_file:
+            json.dump(keyboard, json_file)
+        exit()
+
+    raw, simplified = generate_keyboards(args.simplify, keyboard, args.dataset, args.gensize, args.epsilon,
+                                         args.save_stats)
+    with open(f"keyboards/{args.name}.raw", "w") as json_file:
+        json.dump(raw, json_file)
+    if simplified:
+        with open(f"keyboards/{args.name}.simplified", "w") as json_file:
+            json.dump(simplified, json_file)
 
 
 if __name__ == "__main__":
