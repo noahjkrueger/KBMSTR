09bbfa81e44b1a1cce72135be27a866470907472
Noah Krueger <noahjkrueger@gmail.com>
2022-10-31 16:11:32 -0400

    working collect data

--- python/collect_data.py
+++ python/collect_data.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import os
 import zipfile
 
-FILE_CHAR_LIM = 10
+FILE_CHAR_LIM = 1000000
 
 
 class Logger:
@@ -15,6 +15,7 @@ class Logger:
         self.__prog = tqdm(total=FILE_CHAR_LIM)
         self.__num_files = 0
         self.__dir_name = "".join(choices(ascii_lowercase, k=16))
+        os.mkdir(self.__dir_name)
         print("Generated 0 files. Press (CTRL-C) to stop capture\nProgress to next file:")
 
     def start(self):
@@ -22,7 +23,7 @@ class Logger:
             with Listener(on_press=self._key_event_press) as the_listener:
                 the_listener.join()
         except KeyboardInterrupt:
-            self._store_data()
+            self._store_data(rm=1)
             name = ''.join(choices(ascii_lowercase, k=16))
             zf = zipfile.ZipFile(f"{name}.zip", "w")
             for dirname, subdirs, files in os.walk(self.__dir_name):
@@ -35,25 +36,29 @@ class Logger:
                     os.remove(os.path.join(root, name))
             os.rmdir(self.__dir_name)
 
+    def _rec_key(self, key):
+        self.__prog.update(1)
+        self.__captured.append(key)
 
     def _key_event_press(self, key):
-        print(key)
         if key == Key.space:
-            self.__captured.append(" ")
-        else:
-            self.__prog.update(1)
-            self.__captured.append(key)
+            self._rec_key(" ")
+        elif len(str(key)) == 3:
+            self._rec_key(key)
         if len(self.__captured) >= FILE_CHAR_LIM:
             self._store_data()
 
-    def _store_data(self):
+    def _store_data(self, rm=0):
         os.system('cls' if os.name == 'nt' else 'clear')
+        if rm > 0:
+            self.__captured = self.__captured[0:(-1 * rm)]
         with open(f'{self.__dir_name}/{"".join(choices(ascii_lowercase, k=16))}.txt', 'w') as log:
             for the_key in self.__captured:
                 the_key = str(the_key).replace("'", "")
                 log.write(the_key)
         self.__captured = list()
         self.__prog = tqdm(total=FILE_CHAR_LIM)
+        self.__num_files += 1
         print(f"Generated {self.__num_files} files. Press (CTRL-C) to stop capture\nProgress to next file:")
 
 
